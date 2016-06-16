9a73d06f4dbf7184e522e339fe6c9d2040fd38c5
dan <bauman.85@osu.edu>
2016-06-16 13:42:18 -0500

    iterator

--- pasco/__init__.py
+++ pasco/__init__.py
@@ -3,27 +3,15 @@ from tempfile import NamedTemporaryFile
 import os
 class IndexParser(object):
     def __init__(self):
+        self.delimiter = "||" #from pascohelpermodule.c
+        self.headers="type||url||modified_time||access_time||filename||directory||http_headers".split(self.delimiter)
         return None
     def parse(self, indexfile):
-        return_file = "%s.out" %(indexfile)
-        try:
-            with open(return_file, 'a') as f:
-                f.seek(0)
-        except IOError:
-            with NamedTemporaryFile() as f:
-                return_file = f.name
-        return_file = pascohelper.mainparse(indexfile, return_file )
-        try:
-            with open(return_file, 'rb') as f:
-                headers = f.readline().strip().split("||")
-                line = f.readline().strip().split("||")
-                while line:
-                    yield self.make_dict(headers, line)
-                    line = f.readline().strip().split("||")
-                    if len(line) == 1:
-                        break
-        finally:
-            os.unlink(return_file)
+        generator = pascohelper.iterparse( indexfile )
+        for line in generator:
+            if line:
+                line = line.split(self.delimiter)
+                yield self.make_dict(self.headers, line)
         return
     def make_dict(self, headers, line):
         result = {}
---
+++ testapi.py
@@ -0,0 +1,16 @@
+__author__ = 'dan'
+#import pascohelper
+#print pascohelper.mainparse('/tmp/tmpr41Td2/support.CITRIX/AppData/Local/Microsoft/Windows/Temporary Internet Files/Content.IE5/index.dat', '/tmp/dan.out3' )
+
+from sys import argv
+from pprint import pprint
+import pasco
+
+if len(argv) == 1:
+    filename = '/tmp/tmpr41Td2/support.CITRIX/AppData/Local/Microsoft/Windows/Temporary Internet Files/Content.IE5/index.dat'
+else:
+    filename = argv[1]
+ip = pasco.IndexParser()
+for match in ip.parse(filename):
+    if match:
+        pprint(match)
